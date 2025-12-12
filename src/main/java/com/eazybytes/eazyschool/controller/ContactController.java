ddda4d25229f7f6bd1b001f8d619e8b189c4ae11
Anand Galla <anandgalla78@gmail.com>
2025-12-12 16:53:15 -0600

    Configured spring security dependency

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -48,6 +48,8 @@ public class ContactController {
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
+        contactService.setCounter((contactService.getCounter()) + 1);
+        log.info("Number of times the Contact is submitted: " + contactService.getCounter());
         return "redirect:/contact";
     }
 }
--- src/main/java/com/eazybytes/eazyschool/services/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/services/ContactService.java
@@ -3,10 +3,27 @@ package com.eazybytes.eazyschool.services;
 import com.eazybytes.eazyschool.models.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
+@SessionScope
 public class ContactService {
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    private int counter;
+
+    public ContactService() {
+        System.out.println("Contact Service Bean initialized");
+    }
 
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
